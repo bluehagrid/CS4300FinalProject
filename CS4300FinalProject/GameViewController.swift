777e05c52d643d59b3bbdc3b678f611cf33511df
bluehagrid <jacob.jackson@dixiesuccess.org>
2020-04-15 12:22:33 -0600

    Test to see if commit and push are working.

---
+++ CS4300FinalProject/GameViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GameViewController.swift
+//  CS4300FinalProject
+//
+//  Created by Jacob Jackson on 4/15/20.
+//  Copyright © 2020 Jacob Jackson. All rights reserved.
+//
+
+import UIKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
